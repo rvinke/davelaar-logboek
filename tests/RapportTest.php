cb7c6b9e122ebb5fcab899dbd3745c5fb5efe565
Laravel Shift <shift@laravelshift.com>
2018-08-11 15:24:20 +0000

    Shift tests

--- tests/RapportTest.php
+++ tests/RapportTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class RapportTest extends TestCase
+class RapportTest extends BrowserKitTestCase
 {
 
     /**
